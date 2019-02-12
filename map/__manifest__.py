f92611eec4b2ba3e97bab95d8c7c4f8cdf27cf1d
Manuel Muñoz <heidiaricel@hotmail.com>
2019-02-12 17:53:55 +0100

    Iniciar plugin para odoo
    Iniciar plugin para odoo

---
+++ map/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Plugin Mapa',
+    'version': '0.1',
+    'description': 'Modelo básico para mapas',
+    'category': 'Tools',
+    'summary': 'Modulo para Odoo que utiliza las herramientas Leafletjs y Mapbox para mostrar mapas',
+    'author': 'Manuel Munoz',
+    'depends': ['web'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'data': ['views/menu.xml', 'views/location.xml']
+}
