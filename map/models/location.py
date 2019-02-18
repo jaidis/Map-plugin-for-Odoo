0ac4784bc1239db8a8d40afa92be4704727d31f9
Manuel Muñoz <heidiaricel@hotmail.com>
2019-02-18 19:49:16 +0100

    Añadidos nuevos campos en la localización
    Añadidos nuevos campos en la localización

--- map/models/location.py
+++ map/models/location.py
@@ -4,5 +4,15 @@ from odoo import models, fields, api, _
 class Locations(models.Model):
     _name = 'map.location'
     direccion = fields.Char("Dirección", required=True)
+    poblacion = fields.Char('Población', required=False)
+    provincia = fields.Char('Provincia', required=False)
+    codPostal = fields.Integer('Cod Postal', required=False)
     latitude = fields.Char("Latitude", required=True)
     longitude = fields.Char("Longitude", required=True)
+
+    def name_get(self):
+        res = []
+        for record in self:
+            name = record.direccion + ' - ' + record.poblacion + ' ' + record.provincia
+            res.append((record.id, name))
+        return res
